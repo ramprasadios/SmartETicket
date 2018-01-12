8799be060a22061f4a9556bc25bda70b6ca539f4
Ramprasad A <ramprasad179@gmail.com>
2018-01-12 07:03:05 +0530

    Initial Commit.
    - Initial Project Commit..

---
+++ SmartETicket/SmartETicket/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SmartETicket
+//
+//  Created by Ramprasad A on 11/01/18.
+//  Copyright © 2018 Ramprasad A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
